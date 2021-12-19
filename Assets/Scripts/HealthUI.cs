21e7785f5310228c6f20e8eda4f10f54eb850560
Vraksi <Jakobmetalv@gmail.com>
2021-12-19 15:40:57 +0100

    Init

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    /// <summary>
+    /// REMEBER TO PUT NEW HEARTS IN TO THE LIST IF YOU ADD MORE OF THEM IN THE INSPECTOR
+    /// </summary>
+
+    // Maybe should be private and a function to find them instead
+    public Image[] hearts;
+    public GameObject player;
+    public PlayerMovement playerMovement;
+
+    [SerializeField] int health = 3;
+    [SerializeField] [Range(0, 5)] int maxHealth = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = player.GetComponent<PlayerMovement>();
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].color = new Color(255, 255, 255, 255);
+            }
+            else
+            {
+                hearts[i].color = new Color(255, 255, 255, 0);
+            }
+        }
+
+    }
+
+
+    // should maybe not be able to into MINUS but idc at this moment
+    public void TakeDamage()
+    {
+        health--;
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].color = new Color(255, 255, 255, 255);
+            }
+            else
+            {
+                hearts[i].color = new Color(255, 255, 255, 0);
+            }
+        }
+    }
+
+    public void GainHealth()
+    {
+        if (health >= maxHealth) { return; }
+        health++;
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].color = new Color(255, 255, 255, 255);
+            }
+            else
+            {
+                hearts[i].color = new Color(255, 255, 255, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Running")]
+    [SerializeField] float runSpeed = 5f;
+
+    [Header("Jumping")]
+    [SerializeField] float jumpVelocity = 15f;
+    [SerializeField] [Range(0f, 1f)] float floatingFactor = 1f;
+
+    [Header("Health")]
+    public GameObject healthI;
+    private HealthUI healthScript;
+
+
+    private Rigidbody2D rb2d;
+    private float xScale;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        xScale = transform.localScale.x;
+
+
+
+        healthScript = healthI.GetComponent<HealthUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+        CanIJump();
+        Jump();
+        Tester();
+    }
+
+    private void Tester()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            healthScript.TakeDamage();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            healthScript.GainHealth();
+        }
+    }
+
+    private void Run()
+    {
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * runSpeed, rb2d.velocity.y);
+    }
+
+    private void Jump()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && CanIJump())
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if(rb2d.velocity.y > 0)
+            rb2d.velocity += new Vector2(rb2d.velocity.x, -rb2d.velocity.y * 1f);
+        }
+    }
+
+    // TODO Rename to onGround()
+    //Called in jump to know whether or not the object can jump
+    public bool CanIJump()
+    {
+        // should probably be made into a vector2
+        Vector3 leftSide = new Vector3(transform.localPosition.x - 0.5f, transform.localPosition.y -1f);
+        Vector3 rightSide = new Vector3(transform.localPosition.x + 0.5f, transform.localPosition.y - 1f);
+
+        var leftSidePhys = Physics2D.Raycast(leftSide, new Vector2(0, -0.2f));
+        var rightSidePhys = Physics2D.Raycast(rightSide, new Vector2(0, -0.2f));
+
+        //debugging
+        Debug.DrawRay(leftSide, new Vector2 (0, -0.2f));
+        Debug.DrawRay(rightSide, new Vector2(0, -0.2f));
+        //print("Left side " + leftSidePhys.distance);
+        //print("Right side "+ rightSidePhys.distance);
+
+
+        if(leftSidePhys.distance <= 0 && rightSidePhys.distance <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void FlipSprite()
+    {
+
+        // TODO SKAL GØRES MERE DYNAMISK 
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -xScale;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = xScale;
+        }
+        transform.localScale = characterScale;
+    }
+}
