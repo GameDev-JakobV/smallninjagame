662b7f27e3e2111d3d88499275d08c1680839b50
Vraksi <Jakobmetalv@gmail.com>
2022-04-24 20:00:55 +0200

    WallSliding and wall jumping, With bounds update.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jumping")]
     [SerializeField] float jumpVelocity = 15f;
-    [SerializeField] [Range(0f, 0.5f)] private float coyoteFactor = 1f;
+    [SerializeField][Range(0f, 0.5f)] private float coyoteFactor = 1f;
     [SerializeField] private bool canIJump = false;
     private float tempCoyote = 0f;
     private bool haveJumped = false;
@@ -20,11 +20,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float LengthOfDash = 6f;
     public bool CanDash = true;
     private bool Dashing = false;
-    public float temp;
 
     [Header("Wall Slide")]
     [SerializeField] float SlidingSpeed = -2f;
-    private bool Wall = false;
+    [SerializeField] float WallJumpHeight = 5f;
+    private bool WallSlid = false;
+    private bool WallJumping = false;
+    public float WallJumpTime = 0f;
 
     [Header("Health")]
     public GameObject healthI;
@@ -34,8 +36,14 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;
     private float xScale;
 
-    private Vector3 TopLeft, TopRight, BottomLeft, BottomRight;
+    private Vector3 TopLeft, TopRight, MiddleLeft, MiddleRight, BottomLeft, BottomRight;
+    private RaycastHit2D TopL, TopR, MidL, MidR, BotL, BotR;
+    private Bounds bounds;
+    private float SkinWidth;
+
+
     SpriteRenderer BoxCol;
+    Vector3 CharacterScale;
 
     // Start is called before the first frame update
     void Start()
@@ -44,26 +52,51 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         xScale = transform.localScale.x;
         healthScript = healthI.GetComponent<HealthUI>();
-        DashTime = LengthOfDash / DashSpeed;
-
         BoxCol = GetComponent<SpriteRenderer>();
-        
+        SkinWidth = Physics2D.defaultContactOffset;
+        DashTime = LengthOfDash / DashSpeed;
+        WallJumpTime = BoxCol.bounds.size.x / runSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        BottomRight = new Vector2(transform.localPosition.x + (BoxCol.bounds.size.x / 2), transform.localPosition.y - (BoxCol.bounds.size.y / 2));
-        BottomLeft = new Vector2(transform.localPosition.x - (BoxCol.bounds.size.x / 2), transform.localPosition.y - (BoxCol.bounds.size.y / 2));
-        TopRight = new Vector2(transform.localPosition.x + (BoxCol.bounds.size.x / 2), transform.localPosition.y + (BoxCol.bounds.size.y / 2));
-        TopLeft = new Vector2(transform.localPosition.x - (BoxCol.bounds.size.x / 2), transform.localPosition.y + (BoxCol.bounds.size.y / 2));
+        UpdateBounds();
+        RayCasts();
         Run();
         OnGround();
-        OnWall();   
+        OnWall();
         FlipSprite();
         Jump();
         Dash();
         Tester();
+    }
+
+    private void UpdateBounds()
+    {
+        bounds = BoxCol.bounds;
+        BottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        BottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        MiddleRight = new Vector2(bounds.max.x, bounds.max.y / 2);
+        MiddleLeft = new Vector2(bounds.min.x, bounds.max.y / 2);
+        TopRight = new Vector2(bounds.max.x, bounds.max.y);
+        TopLeft = new Vector2(bounds.min.x, bounds.max.y);
+    }
+
+    private void RayCasts()
+    {
+        TopL = Physics2D.Raycast(TopLeft, Vector2.left, -SkinWidth, LayerMask.GetMask("Ground"));
+        TopR = Physics2D.Raycast(TopRight, Vector2.right, SkinWidth, LayerMask.GetMask("Ground"));
+        BotL = Physics2D.Raycast(BottomLeft, Vector2.down, SkinWidth, LayerMask.GetMask("Ground"));
+        BotR = Physics2D.Raycast(BottomRight, Vector2.down, SkinWidth, LayerMask.GetMask("Ground"));
+    }
+
+    private void Run()
+    {
+        if (Dashing == true) return;
+        if (WallJumping == true) return;
+
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * runSpeed, rb2d.velocity.y);
     }
 
     private void Tester()
@@ -81,7 +114,7 @@ public class PlayerMovement : MonoBehaviour
     //TODO: skal måske gøres med animation 
     private void Dash()
     {
-        if(Input.GetKeyDown(KeyCode.E) && CanDash)
+        if (Input.GetKeyDown(KeyCode.E) && CanDash)
         {
             StartCoroutine(IDash());
         }
@@ -101,94 +134,78 @@ public class PlayerMovement : MonoBehaviour
         yield return null;
     }
 
-    private void Run()
-    {
-        Debug.Log(Input.GetAxisRaw("Horizontal"));
-        if(Dashing == false)
-        {
-            rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * runSpeed, rb2d.velocity.y);
-        }
-    }
-
     private void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && canIJump && coyoteFactor >= 0)
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
-            haveJumped = true;
+            if (WallSlid)
+            {
+                StartCoroutine(IWallJump());
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpVelocity);
+                haveJumped = true;
+            }
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
-            if(rb2d.velocity.y > 0)
-            rb2d.velocity += new Vector2(rb2d.velocity.x, -rb2d.velocity.y * 1f);
+            if (rb2d.velocity.y > 0)
+                rb2d.velocity += new Vector2(0f, -rb2d.velocity.y * 1f);
         }
     }
 
+    private IEnumerator IWallJump()
+    {
+        WallJumping = true;
+        rb2d.velocity = new Vector2(-FacingRight * runSpeed, jumpVelocity);
+        yield return new WaitForSeconds(WallJumpTime);
+        WallJumping = false;
+        yield return null;
+    }
+
+    // i have no idea if this is good but well have to wait and see if some problems arise from it.
     private void OnWall()
     {
-        RaycastHit2D Top = Physics2D.Raycast(TopLeft, new Vector2(-0.2f, 0f), 20f);
-        RaycastHit2D Bottom = Physics2D.Raycast(BottomLeft, new Vector2(-0.2f, 0f), 20f);
-
-        Debug.DrawRay(TopLeft, new Vector2(-0.2f, 0), Color.red);
-        Debug.DrawRay(BottomLeft, new Vector2(-0.2f, 0), Color.blue);
-        Debug.Log(Top.distance);
-        if(Top.distance <= 0.03f && Input.GetAxisRaw("Horizontal") == -1)
+        WallSlid = false;
+        if (TopL.collider is null && TopR.collider is null)
         {
+            return;
+        }
+
+        if (rb2d.velocity.y > 0f)
+        {
+            return;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1 && TopR.collider is null)
+        {
+            CharacterScale.x = xScale;
+            WallSlid = true;
+
             rb2d.velocity = new Vector2(0f, -1f);
-            Debug.Log(rb2d.velocity);
         }
-
-
-        if (transform.localScale.x < 0)
+        if (Input.GetAxisRaw("Horizontal") == 1 && TopL.collider is null)
         {
-            
-
-
+            CharacterScale.x = -xScale;
+            WallSlid = true;
+            rb2d.velocity = new Vector2(0f, -1f);
         }
-        else if (transform.localScale.x > 0)
-        {
-
-        }
-
-        
-        
-        //Debug.DrawRay(BottomRight, new Vector2(0, -2f), Color.red);
-        //Debug.DrawRay(BottomLeft, new Vector2(0, -2f), Color.white);
-        //Debug.DrawRay(TopRight, new Vector2(0, 2f), Color.blue);
-        //Debug.DrawRay(TopLeft, new Vector2(0, 2f), Color.green);
+        transform.localScale = CharacterScale;
     }
 
-    // TODO REFACTOR sådan at variabler ikke instantieres i funktion
+    // TODO REFACTOR sådan at variabler ikke instantieres i funktion, Længden skal være skin width og på om den collider og ikke dens længde
     // Called in jump to know whether or not the object can jump
     // DER KAN IKKE VÆRE GAPS MED INGENTING UNDER, SKAL VÆRE ET OBJEKT FOR AT DET VIRKER
     private void OnGround()
     {
-        // should probably be made into a vector2
-        Vector3 leftSide = new Vector3(transform.localPosition.x - 0.5f, transform.localPosition.y -1f);
-        Vector3 rightSide = new Vector3(transform.localPosition.x + 0.5f, transform.localPosition.y - 1f);
-
-        LayerMask layerMask = LayerMask.GetMask("Ground");
-
-        RaycastHit2D leftSidePhys = Physics2D.Raycast(BottomLeft, new Vector2(0, -0.2f), 20f, layerMask);
-        RaycastHit2D rightSidePhys = Physics2D.Raycast(BottomRight, new Vector2(0, -0.2f), 20f, layerMask);
         Debug.DrawRay(BottomLeft, new Vector2(0, -0.2f), Color.red);
         Debug.DrawRay(BottomRight, new Vector2(0, -0.2f), Color.blue);
 
-        //debugging
-        //Debug.DrawRay(BottomLeft, new Vector2(0, -0.2f), Color.red);
-        //Debug.DrawRay(BottomRight, new Vector2(0, -0.2f), Color.blue);
-        //print("Left side " + leftSidePhys.distance);
-        //print("Right side "+ rightSidePhys.distance);
-        //print(haveJumped);  
-
-        if (leftSidePhys.distance <= 0 && !haveJumped || rightSidePhys.distance <= 0 && !haveJumped)
+        if (BotL.collider is not null && !haveJumped || BotR.collider is not null && !haveJumped)
         {
             tempCoyote = coyoteFactor;
             canIJump = true;
-            if(Dashing == true)
-            {
-                CanDash = false;
-            }
             CanDash = true;
         }
         else
@@ -209,19 +226,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void FlipSprite()
     {
-
+        CharacterScale = transform.localScale;
+        if (WallSlid == true) return;
+        if (WallJumping == true) return;
         // TODO SKAL GØRES MERE DYNAMISK 
-        Vector3 characterScale = transform.localScale;
         if (Input.GetAxis("Horizontal") < 0)
         {
             FacingRight = -1;
-            characterScale.x = -xScale;
+            CharacterScale.x = -xScale;
         }
         if (Input.GetAxis("Horizontal") > 0)
         {
             FacingRight = 1;
-            characterScale.x = xScale;
+            CharacterScale.x = xScale;
         }
-        transform.localScale = characterScale;
+        transform.localScale = CharacterScale;
     }
 }
